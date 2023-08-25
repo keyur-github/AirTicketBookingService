6e417fd8bd16579c9982fbd56d233ded12d22462
Keyur Panchal <keyur.np@somaiya.edu>
2023-08-26 02:19:10 +0530

    Fixed minor things of Rabbit MQ

--- src/controllers/booking-controller.js
+++ src/controllers/booking-controller.js
@@ -13,10 +13,16 @@ class BookingController {
 
     async sendMessageToQueue(req, res) {
         const channel = await createChannel(); 
-        const data = {
-            message: "Success"
+        const payload = {
+            data: {
+                subject: "This is notification from queue",
+                content: "Some queue will subscribe this",
+                recepientEmail: "keyur.np@somaiya.edu",
+                notificationTime: Date()
+            },
+            service: "CREATE_TICKET"
         }
-        publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(data));
+        publishMessage(channel, REMINDER_BINDING_KEY, JSON.stringify(payload));
         return res.status(200).json({
             message: "Successfully published the event"
         });
